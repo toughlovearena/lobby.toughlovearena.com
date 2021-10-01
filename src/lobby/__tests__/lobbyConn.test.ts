e761fd8aa6f94634546eb5fdb416f85cdb3c7359
mpaulweeks <mpaulweeks@gmail.com>
2021-10-01 13:43:14 -0400

    improve conn test coverage

--- src/lobby/__tests__/lobbyConn.test.ts
+++ src/lobby/__tests__/lobbyConn.test.ts
@@ -1,18 +1,69 @@
+import { HostRemoveMod, HostUpdateSettings, LobbyPlayerStatus, MessageReg, MessageType, SocketMessage, UpdateStatus, UploadMod } from '../../types';
 import { LobbyConnection } from '../lobbyConn';
 import { LobbyManager } from '../lobbyManager';
-import { EmptyCallback, FakeLobbyManager, genUploadMod } from './__mocks__/testHelpers';
+import { FakeLobbyManager, genUploadMod } from './__mocks__/testHelpers';
 
 describe('LobbyConnection', () => {
-  test('handleMessage stops working after leave() is called', () => {
-    const fakeLobby = new FakeLobbyManager();
-    let leaveCount = 0;
-    const sut = new LobbyConnection({
+
+  let fakeLobby: FakeLobbyManager;
+  let leaveCount: number;
+  let inbox: SocketMessage[];
+  let sut: LobbyConnection;
+
+  beforeEach(() => {
+    fakeLobby = new FakeLobbyManager();
+    leaveCount = 0;
+    inbox = [];
+    sut = new LobbyConnection({
       clientId: 'c1',
       lobby: fakeLobby as any as LobbyManager,
-      cb: EmptyCallback,
+      cb: sm => inbox.push(sm),
       onLeave: () => leaveCount++,
     });
+  });
 
+  test('handleMessage passes through to LobbyManager', () => {
+    const updateStatus: UpdateStatus = {
+      type: MessageType.UpdateStatus,
+      status: LobbyPlayerStatus.Spectate,
+    };
+    sut.handleMessage(updateStatus);
+    expect(fakeLobby._updateStatus).toStrictEqual([updateStatus.status]);
+
+    const hostUpdateSettings: HostUpdateSettings = {
+      type: MessageType.HostUpdateSettings,
+      patch: {},
+    };
+    sut.handleMessage(hostUpdateSettings);
+    expect(fakeLobby._hostUpdateSettings).toStrictEqual([hostUpdateSettings.patch]);
+
+    const uploadMod: UploadMod = {
+      type: MessageType.UploadMod,
+      data: { modId: 'dne', configJson: 'json', },
+    };
+    sut.handleMessage(uploadMod);
+    expect(fakeLobby._uploadMod).toStrictEqual([uploadMod.data]);
+
+    const hostRemoveMod: HostRemoveMod = {
+      type: MessageType.HostRemoveMod,
+      modId: 'dne',
+    };
+    sut.handleMessage(hostRemoveMod);
+    expect(fakeLobby._hostRemoveMod).toStrictEqual([hostRemoveMod.modId]);
+  });
+
+  test('handleMessage sends error when type is unsupported', () => {
+    const badMessage: MessageReg = {
+      type: MessageType.Register,
+      lobbyId: 'l1',
+      tag: 't1',
+    };
+    sut.handleMessage(badMessage);
+    expect(inbox.length).toBe(1);
+    expect(inbox[0].type).toBe(MessageType.Error);
+  });
+
+  test('handleMessage stops working after leave() is called', () => {
     expect(fakeLobby._uploadMod.length).toBe(0);
     expect(leaveCount).toBe(0);
 
